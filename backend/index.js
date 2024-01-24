dbee669fdc3030042f70c441899ec843019f6b10
indrajith <indrajithpg10@gmail.com>
2024-01-24 20:28:18 +0530

    basic backend stuffs

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const db = require('./db');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/api/create', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    db.query("INSERT INTO `users` (`uname`, `password`, `timestamp`) VALUES (?, ?, current_timestamp());", [username, password], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+            console.log(result);
+            res.json(result);
+        }
+    });
+});
+
+app.get('/api/users',(req,res)=>{
+    const username=req.body.username;
+    db.query("select * from users where uname=?",username,(err,result)=>{
+        if(err){
+            console.log(err);
+        }
+        res.json(result);
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
